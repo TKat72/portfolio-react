24a9573717acbce6b17d98e9708a95f3cece5722
Kateryna <tsymbal.katena@mail.ru>
2022-06-28 05:26:12 -0500

    added link

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://TKat72.github.io',
+  homepage: 'https://main--sparkling-lily-63070f.netlify.app',
   title: 'JS.',
 }
 
