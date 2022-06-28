dbb0c848f6be28c897f7632a4e2d7f6456bac22a
Kateryna <tsymbal.katena@mail.ru>
2022-06-28 05:23:44 -0500

    deleted footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
+
 import './App.css'
 
 const App = () => {
